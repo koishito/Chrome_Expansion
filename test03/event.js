eafc14c4ac96fd675fc7a5c8da836102e1b96563
koishito <64292509+koishito@users.noreply.github.com>
2020-11-21 00:17:45 +0900

    Update event.js

--- test03/event.js
+++ test03/event.js
@@ -1,4 +1,18 @@
-alert("test03");
+window.onload = function() {
+  alert('onload');
+}
+  alert('test03');
 
-document.body.addEventListener('keydown', (event) => {console.log(event.key);})
+//document.body.addEventListener('keydown', (event) => {console.log(event.key);}) //console_ok
 
+//windows.addEventListener('keydown',  function(){console.log(event.key);}) //console_error
+
+//document.body.addEventListener('keydown',  function(){console.log(event.key);}) //console_ok
+
+//document.body.addEventListener('click', (event) => {console.log("click");}) //console_ok
+
+//document.body.addEventListener('click', function(){console.log("click");}) //console_ok
+
+//window.addEventListener('scroll', function(){console.log("scroll");}) //console_ok
+
+//document.body.addEventListener('scroll', function(){console.log("scroll");}) //console_ng
